411ca1627567cd2c74163bfd62a628c688255988
Marcos Bazbih <marcosbaza100@gmail.com>
2021-11-13 19:14:36 +0200

    added update option

--- js/addMovie.js
+++ js/addMovie.js
@@ -19,7 +19,7 @@ class Movie {
 postMovieForm.onsubmit = (event) => {
     event.preventDefault();
     
-    let movie = new Movie(movieName.value, image.value, synopsis.value, linkToMovie.value, Number(rating.value))
+    let movie = new Movie(postMovieName.value, postImage.value, postSynopsis.value, postLinkToMovie.value, Number(postRating.value))
     let options = {
         method: "POST",
         body: JSON.stringify({ movie }),
@@ -27,4 +27,18 @@ postMovieForm.onsubmit = (event) => {
     }
     restApiById(SAVE_MOVIE_ENDPOINT, options)
         .finally(alert("your movie has been added"))
+}
+
+
+updateMovieForm.onsubmit = (event) => {
+    event.preventDefault();
+    
+    let movie = new Movie(putMovieName.value, putImage.value, putSynopsis.value, putLinkToMovie.value, Number(putRating.value))
+    let options = {
+        method: "PUT",
+        body: JSON.stringify({ movie }),
+        headers: { 'Content-Type': 'application/json' }
+    }
+    restApiById(BY_ID_ENDPOINT+putMovieId.value, options)
+        .finally(alert("your movie has been updated"))
 }
