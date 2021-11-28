968a942915a176f1793599e1da2fdf7d06c29a1d
Marcos Bazbih <marcosbaza100@gmail.com>
2021-11-28 19:50:34 +0200

    75%

--- js/moviesTable.js
+++ js/moviesTable.js
@@ -1,5 +1,3 @@
-slowDownBgVideo();
-
 getFromApi(tableBox, BASIC_API+"all")
     .then(res => addMovieToTable(res.data, tableBody))
 
@@ -7,14 +5,14 @@ getFromApi(tableBox, BASIC_API+"all")
 function addMovieToTable(arr, table) {
     for (let item of arr) {
         table.innerHTML +=
-            `<tr>
-                <td>${item._id}</td>
-                <td>${item.movieName}</td>
-                <td>${item.image}</td>
-                <td>${item.synopsis}</td>
-                <td><a href=${item.linkToMovie} target="blank">more info</a></td>
-                <td>${item.rating}/10</td>
-                <td>${item.date}</td>
+            `<tr class="table-row">
+                <td class="table-td">${item._id}</td>
+                <td class="table-td">${item.movieName}</td>
+                <td class="table-td">${item.image}</td>
+                <td class="table-td">${item.synopsis}</td>
+                <td class="table-td td-link-box"><a class="table-td-link" href=${item.linkToMovie} target="blank">more info</a></td>
+                <td class="table-td">${item.rating}/10</td>
+                <td class="table-td">${item.date}</td>
             </tr>`
     }
 }
